535129224e3e9fc23433434d0607e2ec344b4400
ino-cpu <inostore25@gmail.com>
2026-08-05 17:45:45 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "am-premium-creator",
+  "version": "1.0.0",
+  "description": "Buat akun Alight Motion Premium - By Ino.Digital",
+  "scripts": {
+    "dev": "vercel dev",
+    "deploy": "vercel --prod"
+  },
+  "dependencies": {
+    "bycf": "latest"
+  },
+  "devDependencies": {
+    "vercel": "^32.0.0"
+  }
+}
