6684a103541fe5008dc018093f30d22be7435942
ino-cpu <inostore25@gmail.com>
2026-08-05 18:22:56 +0700

    Update amprem.js

--- amprem.js
+++ amprem.js
@@ -1,1 +1,19 @@
 
+// Di bagian fullAutoWorkflow(), tambahkan instruksi lebih jelas:
+
+console.log('');
+console.log('═'.repeat(62));
+console.log('📱 *LANJUTKAN KE APLIKASI ALIGHT MOTION*');
+console.log('═'.repeat(62));
+console.log('');
+console.log(`📧 *Email Anda:* ${tempEmail}`);
+console.log('');
+console.log('🔴 *SEKARANG BUKA APLIKASI AM DI HP:*');
+console.log('   1. Buka aplikasi Alight Motion');
+console.log('   2. Pilih "Masuk dengan Email"');
+console.log(`   3. Masukkan email: ${tempEmail}`);
+console.log('   4. Klik "Kirim Link Login"');
+console.log('');
+console.log('⏳ *Script akan otomatis menangkap link login...*');
+console.log('   (Tunggu maksimal 4 menit)');
+console.log('');
